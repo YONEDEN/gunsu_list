45af80e76b4e3e9464c861260cf556c7997c37c1
YONEDEN <jyl2142@gmail.com>
2015-11-08 00:15:24 +0900

    もう一度並べた

--- gunsu_list2/Form1.Designer.cs
+++ gunsu_list2/Form1.Designer.cs
@@ -50,7 +50,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Location = new System.Drawing.Point(8, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
@@ -59,7 +59,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(379, 17);
+            this.label2.Location = new System.Drawing.Point(375, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 1;
@@ -68,7 +68,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Location = new System.Drawing.Point(8, 99);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 2;
@@ -77,7 +77,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(613, 65);
+            this.label4.Location = new System.Drawing.Point(609, 58);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(77, 12);
             this.label4.TabIndex = 3;
@@ -86,7 +86,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(613, 99);
+            this.label5.Location = new System.Drawing.Point(542, 99);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 12);
             this.label5.TabIndex = 4;
@@ -104,7 +104,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(610, 144);
+            this.checkBox1.Location = new System.Drawing.Point(544, 144);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(88, 16);
             this.checkBox1.TabIndex = 7;
@@ -126,7 +126,7 @@
             "表紙イラスト",
             "表紙デザイン",
             "編集"});
-            this.listBox1.Location = new System.Drawing.Point(351, 96);
+            this.listBox1.Location = new System.Drawing.Point(265, 96);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(118, 64);
             this.listBox1.TabIndex = 8;
@@ -162,7 +162,7 @@
             "15年12月号",
             "16年01月号",
             "16年02月号"});
-            this.listBox2.Location = new System.Drawing.Point(65, 96);
+            this.listBox2.Location = new System.Drawing.Point(61, 96);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(179, 64);
             this.listBox2.TabIndex = 9;
@@ -178,7 +178,7 @@
             "ハウツー",
             "イラスト",
             "コミック"});
-            this.listBox3.Location = new System.Drawing.Point(475, 96);
+            this.listBox3.Location = new System.Drawing.Point(399, 96);
             this.listBox3.Name = "listBox3";
             this.listBox3.Size = new System.Drawing.Size(127, 64);
             this.listBox3.TabIndex = 10;
@@ -187,7 +187,7 @@
             // 
             this.listBox4.FormattingEnabled = true;
             this.listBox4.ItemHeight = 12;
-            this.listBox4.Location = new System.Drawing.Point(65, 13);
+            this.listBox4.Location = new System.Drawing.Point(61, 54);
             this.listBox4.Name = "listBox4";
             this.listBox4.Size = new System.Drawing.Size(308, 16);
             this.listBox4.TabIndex = 11;
@@ -196,14 +196,14 @@
             // 
             this.listBox5.FormattingEnabled = true;
             this.listBox5.ItemHeight = 12;
-            this.listBox5.Location = new System.Drawing.Point(426, 13);
+            this.listBox5.Location = new System.Drawing.Point(422, 54);
             this.listBox5.Name = "listBox5";
             this.listBox5.Size = new System.Drawing.Size(181, 16);
             this.listBox5.TabIndex = 12;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(696, 58);
+            this.textBox1.Location = new System.Drawing.Point(692, 54);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(59, 19);
             this.textBox1.TabIndex = 13;
@@ -218,7 +218,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(694, 13);
+            this.button1.Location = new System.Drawing.Point(694, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(60, 26);
             this.button1.TabIndex = 15;
@@ -227,7 +227,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(228, 51);
+            this.button2.Location = new System.Drawing.Point(602, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 26);
             this.button2.TabIndex = 16;
@@ -236,7 +236,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(696, 96);
+            this.textBox2.Location = new System.Drawing.Point(602, 96);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(59, 19);
             this.textBox2.TabIndex = 17;
